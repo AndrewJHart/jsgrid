ac44aeb3dd188bf7d66ddd63689ec6c98d4b84db
Cory Simmons <cory@mojotech.com>
2014-08-16 21:31:37 -0400

    Get rid of Flexbox in favor of calc
    Add media queries

--- js/grid.js
+++ js/grid.js
@@ -1,31 +1,97 @@
 $(function() {
 
-
-  var gutter = 2,
-      parser = new UAParser(),
-      browser = parser.getBrowser();
-
-  $.each($('[desktop]'), function(i) {
-
-
-    if(browser.name === 'Firefox') {
-      document.styleSheets[0].insertRule('[desktop]:nth-child('+ (i + 1) +') { width: '+ eval($(this).attr('desktop')) * 100 + '%', 0);
-    } else {
-      document.styleSheets[0].addRule('[desktop]:nth-child('+ (i + 1) +')', 'width: '+ eval($(this).attr('desktop')) * 100 + '%'); // works in ie8+
+  var grid = {
+    gutter: 1,
+    breakpoints: {
+      desktop: 1200,
+      tablet: 800,
+      mobile: 500
     }
+  };
+
+  var setGrid = function() {
 
 
+    $.each($('[nm]'), function(i) {
+      var i = i + 1,
+          g = $(this).attr('g');
+
+      if($(window).width() > grid.breakpoints.desktop) {
+
+        document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (grid.gutter / 2) +'rem; margin-right: -'+ (grid.gutter / 2) +'rem');
+        $('[d]').ready(function() {
+          document.styleSheets[0].addRule('[d]', 'margin-left: '+ (grid.gutter / 2) +'rem; margin-right: '+ (grid.gutter / 2) +'rem');
+        });
+        $.each($('[d]'), function(i) {
+          document.styleSheets[0].addRule('[d]:nth-child('+ (i + 1) +')', 'width: calc('+ (eval($(this).attr('d')) * 100) + '% - '+ grid.gutter +'rem)');
+        });
+
+        if(g !== '') {
+          document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (g / 2) +'%; margin-right: -'+ (g / 2) +'%');
+          $('[d]').ready(function() {
+            document.styleSheets[0].addRule('[d]', 'margin-left: '+ (g / 2) +'%; margin-right: '+ (g / 2) +'%');
+          });
+          $.each($('[d]'), function(i) {
+            document.styleSheets[0].addRule('[d]:nth-child('+ (i + 1) +')', 'width: '+ ((eval($(this).attr('d')) * 100) - (g)) + '%');
+          });
+        }
+
+      } else if($(window).width() > grid.breakpoints.tablet) {
+
+        document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (grid.gutter / 2) +'rem; margin-right: -'+ (grid.gutter / 2) +'rem');
+        $('[t]').ready(function() {
+          document.styleSheets[0].addRule('[t]', 'margin-left: '+ (grid.gutter / 2) +'rem; margin-right: '+ (grid.gutter / 2) +'rem');
+        });
+        $.each($('[t]'), function(i) {
+          document.styleSheets[0].addRule('[t]:nth-child('+ (i + 1) +')', 'width: calc('+ (eval($(this).attr('t')) * 100) + '% - '+ grid.gutter +'rem)');
+        });
+
+        if(g !== '') {
+          document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (g / 2) +'%; margin-right: -'+ (g / 2) +'%');
+          $('[t]').ready(function() {
+            document.styleSheets[0].addRule('[t]', 'margin-left: '+ (g / 2) +'%; margin-right: '+ (g / 2) +'%');
+          });
+          $.each($('[t]'), function(i) {
+            document.styleSheets[0].addRule('[t]:nth-child('+ (i + 1) +')', 'width: '+ ((eval($(this).attr('t')) * 100) - (g)) + '%');
+          });
+        }
+
+      } else {
+
+        document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (grid.gutter / 2) +'rem; margin-right: -'+ (grid.gutter / 2) +'rem');
+        $('[m]').ready(function() {
+          document.styleSheets[0].addRule('[m]', 'margin-left: '+ (grid.gutter / 2) +'rem; margin-right: '+ (grid.gutter / 2) +'rem');
+        });
+        $.each($('[m]'), function(i) {
+          document.styleSheets[0].addRule('[m]:nth-child('+ (i + 1) +')', 'width: calc('+ (eval($(this).attr('m')) * 100) + '% - '+ grid.gutter +'rem)');
+        });
+
+        if(g !== '') {
+          document.styleSheets[0].addRule('[nm]', 'margin-left: -'+ (g / 2) +'%; margin-right: -'+ (g / 2) +'%');
+          $('[m]').ready(function() {
+            document.styleSheets[0].addRule('[m]', 'margin-left: '+ (g / 2) +'%; margin-right: '+ (g / 2) +'%');
+          });
+          $.each($('[m]'), function(i) {
+            document.styleSheets[0].addRule('[m]:nth-child('+ (i + 1) +')', 'width: '+ ((eval($(this).attr('m')) * 100) - (g)) + '%');
+          });
+        }
+
+      }
+    });
+
+  }
+
+  setGrid();
+
+  var didResize = false;
+  $(window).resize(function() {
+    didResize = true;
   });
-
-  $('[desktop]').ready(function() {
-    if(browser.name === 'Firefox') {
-      document.styleSheets[0].insertRule('[desktop] { margin-right: '+ gutter +'rem', 0);
-      document.styleSheets[0].insertRule('[desktop]:last-child { margin-right: 0', 0);
-    } else {
-      document.styleSheets[0].addRule('[desktop]', 'margin-right: '+ gutter + 'rem');
-      document.styleSheets[0].addRule('[desktop]:last-child', 'margin-right: 0');
+  setInterval(function() {
+    if(didResize) {
+      didResize = false;
+      setGrid();
     }
-  });
-
+  }, 500);
 
 });
